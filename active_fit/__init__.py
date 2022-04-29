ec6412cd0ab30c724ea99d7547f69b16922e85c3
Bharath Poojary <bharathbillawa@gmail.com>
2022-04-30 01:44:19 +0200

    Add django scaffolding

---
+++ active_fit/__init__.py
@@ -0,0 +1,7 @@
+"""
+Base Template.
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Bharath Poojary'
+__email__ = 'bharathbillawa@gmail.com'
---
+++ active_fit/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ActiveFitConfig(AppConfig):
+    name = 'active_fit'
