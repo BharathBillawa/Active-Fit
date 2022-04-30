79ceb55300915ea1e54ad4b4e3a32d518466ec01
Bharath Poojary <bharathbillawa@gmail.com>
2022-04-30 14:22:57 +0200

    Add pylint config

--- active_fit/apps.py
+++ active_fit/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-
 class ActiveFitConfig(AppConfig):
+    """
+    Application config for Active Fit.
+
+    Args:
+        name: Name of the application.
+    """
     name = 'active_fit'
